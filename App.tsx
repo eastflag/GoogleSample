46045c663639058c9dfb4ff90774d2c7ab5b4733
eastflag <eastflag@gmail.com>
2022-05-29 16:49:24 +0900

    .

--- App.tsx
+++ App.tsx
@@ -10,13 +10,45 @@
 
 import React from 'react';
 import {Button, SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import {GoogleSignin, statusCodes} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/drive.readonly'], // [Android] what API you want to access on behalf of the user, default is email and profile
+  webClientId: '784124912483-rv3qfle5ona18dn8ujgadj5io705nfm0.apps.googleusercontent.com',
+  // webClientId:
+  //   '784124912483-abs461p8kl0b98lq7s53pm08cgs3l2oj.apps.googleusercontent.com',
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+});
 
 const App = () => {
+  // Somewhere in your code
+  const signIn: any = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('SIGN_IN_CANCELLED');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+        console.log('IN_PROGRESS');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log('PLAY_SERVICES_NOT_AVAILABLE');
+      } else {
+        // some other error happened
+        console.log('error:' + error);
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentInsetAdjustmentBehavior="automatic" style={{flex: 1}}>
         <View>
-          <Button title="google signin"></Button>
+          <Button title="google signin" onPress={() => signIn()}></Button>
         </View>
       </ScrollView>
     </SafeAreaView>
